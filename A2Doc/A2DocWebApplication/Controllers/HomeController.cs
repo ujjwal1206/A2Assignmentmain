c35f6476150ff653af66f90aa516b33f7d58ff69
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2025-02-27 20:17:23 +0530

    Add project files.

---
+++ A2Doc/A2DocWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using A2DocWebApplication.Models;
+
+namespace A2DocWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RestServiceClient _restServiceClient = new RestServiceClient();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult>SecretNumberAsync(int x, int y)
+        {
+            int secNum = await _restServiceClient.SecretNumberAsync(x, y);
+            return Json(new { result = secNum });
+        }
+        [HttpPost]
+        public async Task<JsonResult>CheckNumberAsync(int x, int y)
+        {
+            string checkNum = await _restServiceClient.CheckNumberAsync(x, y);
+            return Json(new { result = checkNum });
+        }
+       
+    }
+}
---
+++ A2Doc/A2DocWebApplication/Models/RestServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace A2DocWebApplication.Models
+{
+    public class RestServiceClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _serviceUrl = "http://localhost:50107/Service1.svc";
+
+        public RestServiceClient()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<int> SecretNumberAsync(int x, int y)
+        {
+            var response = await _httpClient.GetStringAsync($"{_serviceUrl}/secretnumber?x={x}&y={y}");
+            return int.Parse(response);
+        }
+        public async Task<string>CheckNumberAsync(int x,int y)
+        {
+            return await _httpClient.GetStringAsync($"{ _serviceUrl}/checknumber?x={x}&y={y}");
+        }
+
+    }
+}
